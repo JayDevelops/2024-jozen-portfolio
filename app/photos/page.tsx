579cab74438ed61d2e9506602b66339808d29db7
Jay.develops <jaymations1234@gmail.com>
2024-01-11 22:16:59 -0800

    Added layout for metadata on all pages

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,7 +1,14 @@
+import type {Metadata} from "next";
+
 export default function PhotosPage() {
     return (
         <div>
             Photos Page...
         </div>
     )
+}
+
+export const metadata: Metadata = {
+    title: "Videos",
+    description: 'Jozen\'s photos.',
 }
--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,7 +1,14 @@
+import type {Metadata} from "next";
+
 export default function VideoPage() {
     return (
         <div>
             Videos Page...
         </div>
     )
+}
+
+export const metadata: Metadata = {
+    title: "Photos",
+    description: 'Jozen\'s videos.',
 }
